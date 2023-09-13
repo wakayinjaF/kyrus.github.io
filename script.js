fd1d66d0fee1b2dc8b164e06f02b0b3d07839e7d
wakayinja frank <142891493+wakayinjaF@users.noreply.github.com>
2023-09-13 09:20:53 +0300

    Added new files

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    menu.classList.toggle('open');
+};
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    menu.classList.remove('open');
+};
+let darkmode=document.querySelector('#darkmode');
+darkmode.onclick=()=>{
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('dark');
+    }
+    else{
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('dark')
+    }
+}
